4224e864551dcdcc6cb4562c07f9c85bd26cab3e
Poresh2222 <st.yahor.poresh@gmail.com>
2021-04-04 12:54:20 +0200

    Theme Provider + constans(routers)

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,39 @@
-import 'package:flutter/material.dart';
+//import 'dart:js';
 
-import 'package:waterproject_v3/ui/home_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:waterproject_v3/ui/ui.dart';
+import 'package:waterproject_v3/services/services.dart';
+import 'package:waterproject_v3/constants/constants.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider<ThemeProvider>(
+        create: (context) => ThemeProvider(),
+      )
+    ],
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'WaterProject',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new HomeUI(),
-    );
+    return Consumer<ThemeProvider>(builder: (_, themeProviderRef, __) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: Routes.routes,
+        theme: AppThemes.lightTheme,
+        darkTheme: AppThemes.darkTheme,
+        themeMode:
+            themeProviderRef.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+        home:
+            HomeUI(), //(userSnapshot?.data?.uid != null) ? HomeUI() : SignInUI(),
+      );
+    });
   }
 }
